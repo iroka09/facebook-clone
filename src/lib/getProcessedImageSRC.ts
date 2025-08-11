c6ae01a69b8e047aeb66ba3eb2ffaa3796ea43e0
Iroka Ntomchukwu Chisom <irokirokatochukwu@gmail>
2025-08-11 07:42:11 +0100

    ff

--- src/lib/getProcessedImageSRC.ts
+++ src/lib/getProcessedImageSRC.ts
@@ -2,9 +2,7 @@
 // Client-side usage example
 export function getProcessedImageSRC(originalUrl: string, q = 10): string {
   //return originalUrl
-  if (process.env.NODE_ENV === "production") {
-    const url = `/api/process_image?q=${q}url=${encodeURIComponent(originalUrl)}`;
-    return url
-  }
+  if (process.env.NODE_ENV === "production")
+    return `/api/process_image?q=${q}url=${originalUrl}`
   return originalUrl
 }
