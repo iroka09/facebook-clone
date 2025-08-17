fdb80b2a9c0f03551abe35f772b6f5176d438e81
Iroka Ntomchukwu Chisom <irokirokatochukwu@gmail>
2025-08-17 07:37:56 +0100

    ds

--- src/app/api/process_image/route.ts
+++ src/app/api/process_image/route.ts
@@ -2,54 +2,54 @@ import { NextResponse, NextRequest } from 'next/server';
 import sharp from 'sharp';
 
 
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 
+
+
+async function getImage(imageUrl, quality) {
+  const imageResponse = await fetch(imageUrl, { cache: "no-store" });
+  if (!imageResponse.ok) {
+    throw new Error(`Failed to fetch image: ${imageResponse.statusText}`);
+  }
+  // Get the image buffer
+  const arrayBuffer = await imageResponse.arrayBuffer();
+  const buffer = globalThis.Buffer.from(arrayBuffer);
+  // Process with Sharp
+  const processedImage = await sharp(buffer)
+    .jpeg({ mozjpeg: true, quality })
+    .toBuffer();
+  // Return the processed image
+  return new NextResponse(processedImage, {
+    headers: {
+      'Content-Type': 'image/jpeg',
+      //'Cache-Control': 'no-store, max-age=0'
+    },
+  });
+}
 
 
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = req.nextUrl
     const imageUrl = searchParams.get('url');
-    const quality = Number(searchParams.get('q')) || 50;
-    if (!imageUrl) {
-      return NextResponse.json({ error: 'Image URL is required' }, {
-        status: 400
-      });
-    }
+    const quality = Number(searchParams.get('q'));
+    if (!imageUrl) throw Error('Image URL is required')
     // Validate URL format
-    try {
-      new URL(imageUrl);
-    }
-    catch (e) {
-      console.log(e)
-      return NextResponse.json({ error: 'Invalid URL format' }, {
-        status: 400
-      });
-    }
+    new URL(imageUrl);
     // Fetch the image with native fetch()
-    const imageResponse = await fetch(imageUrl, { cache: "no-store" });
-    if (!imageResponse.ok) {
-      throw new Error(`Failed to fetch image: ${imageResponse.statusText}`);
-    }
-    // Get the image buffer
-    const arrayBuffer = await imageResponse.arrayBuffer();
-    const buffer = globalThis.Buffer.from(arrayBuffer);
-    // Process with Sharp
-    const processedImage = await sharp(buffer)
-      .jpeg({ mozjpeg: true, quality })
-      .toBuffer();
-    // Return the processed image
-    return new NextResponse(processedImage, {
-      headers: {
-        'Content-Type': 'image/jpeg',
-        //'Cache-Control': 'no-store, max-age=0'
-      },
-    });
+    return await getImage(imageUrl, quality || 50)
   }
   catch (e) {
-    console.error('Error processing image: ', e);
-    return NextResponse.json({ error: 'Failed to process image' }, {
-      status: 500
-    });
+    console.error('Error processing ORIGINAL image: ', e);
+    try {
+      const fallbackPhotoUrl = "https://www.mountainmotorvehicles.co.uk/wp-content/uploads/2024/05/No-image-available-2.jpg"
+      return await getImage(fallbackPhotoUrl, 100)
+    }
+    catch (e) {
+      console.error('Error processing fallback image: ', e);
+      return NextResponse.json({ error: 'Failed to process image' }, {
+        status: 500
+      });
+    }
   }
 }
--- src/components/AddFriendsFeed.tsx
+++ src/components/AddFriendsFeed.tsx
@@ -9,10 +9,18 @@ import { FaChevronRight } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { HiUserAdd } from "react-icons/hi";
 import { IoMdClose } from "react-icons/io";
+import { AnimatePresence, motion } from "framer-motion";
 
 
+interface AppProps {
+  people: UserDocument[],
+  mutuals: UserDocument[],
+  totalMutual: number[],
+  withRemoveButton: boolean
+}
 
-export default function AddFriendsFeed({ people: _people, mutuals, totalMutual, withRemoveButton }: { people: UserDocument[], mutuals: UserDocument[], totalMutual: number[], withRemoveButton: boolean }) {
+
+export default function AddFriendsFeed({ people: _people, mutuals, totalMutual, withRemoveButton }: AppProps) {
   const [people, setPeople] = useState(_people)
   const containerRef = useRef<HTMLUListElement>(null)
   const removePerson = (email: string) => {
@@ -29,24 +37,15 @@ export default function AddFriendsFeed({ people: _people, mutuals, totalMutual, 
   useEffect(() => {
     const addFriendsBox = containerRef.current
     const card = addFriendsBox?.firstElementChild as (HTMLLIElement | null)
-    const cardWidth = Number(card?.offsetWidth) / 2
     if (addFriendsBox && card) {
       function trigger(e: MouseEvent) {
         const clicked = e.target as HTMLElement
-        if (clicked.tagName === "BUTTON") {
-          const email = clicked.dataset.email
-          const isAddButton = clicked.classList.contains("add_friend_button")
-          const isCancelButton = clicked.classList.contains("cancel_req")
-          const isRemoveButton = clicked.classList.contains("remove_card")
-          if (isRemoveButton) removePerson(email)
-          else sendRequest(email, isAddButton)
-          if (isAddButton) {
-            addFriendsBox!.scrollBy({
-              left: cardWidth,
-              top: 0,
-              behavior: "smooth"
-            })
-          }
+        if (clicked.tagName === "BUTTON" && clicked.classList.contains("add_friend_button")) {
+          addFriendsBox!.scrollBy({
+            left: Number(card?.offsetWidth) / 2,
+            top: 0,
+            behavior: "smooth"
+          })
         }
       }
       addFriendsBox.addEventListener("click", trigger)
@@ -63,49 +62,67 @@ export default function AddFriendsFeed({ people: _people, mutuals, totalMutual, 
         <IoIosMore className="text-3xl active:bg-slate-200 dormant-btn" />
       </div>
       <ul ref={containerRef} className="whitespace-nowrap overflow-x-auto space-x-3 snap-x snap-mandatory px-3">
-        {people.map((person, i) => {
-          const mutualPic = mutuals[i] ? mutuals[i].picture.thumbnail : mutuals[0].picture.thumbnail
-          return (
-            <li key={person.email} className="inline-flex flex-col w-[260px] snap-center rounded-lg overflow-hidden border">
-              <img src={person.picture.large} className="w-full aspect-square" />
-              <div className="flex-1 p-3 space-y-2">
-                <h4 className="text-lg font-bold capitalize">
-                  {person.name.first} {person.name.last}
-                </h4>
-                <div className="flex items-center gap-1 opacity-70">
-                  <img src={mutualPic} className="w-5 aspect-square rounded-full border" />
-                  <span className="text-sm">{totalMutual[i]} mutual friends</span>
+        <AnimatePresence >
+          {people.map((person, i) => {
+            const mutualPic = mutuals[i] ? mutuals[i].picture.thumbnail : mutuals[0].picture.thumbnail
+            return (
+              <motion.li
+                key={person.email}
+                className="inline-flex flex-col snap-center rounded-lg overflow-hidden border"
+                initial={{
+                  opacity: 1,
+                  width: 260
+                }}
+                exit={{
+                  opacity: 0,
+                  width: 0,
+                  transition: {
+                    opacity: { duration: 0.2 },
+                    width: { duration: 0.3, delay: 0.2 }
+                  }
+                }}
+              >
+                <img src={person.picture.large} className="w-full aspect-square" />
+                <div className="flex-1 p-3 space-y-2">
+                  <h4 className="text-lg font-bold capitalize">
+                    {person.name.first} {person.name.last}
+                  </h4>
+                  <div className="flex items-center gap-1 opacity-70">
+                    <img src={mutualPic} className="w-5 aspect-square rounded-full border" />
+                    <span className="text-sm">{totalMutual[i]} mutual friends</span>
+                  </div>
+                  {withRemoveButton ? (
+                    <div className="mt-4 flex justify-between flex-1 items-end justify-between *:text-sm">
+                      {(person.sent_friend_request) ?
+                        <Button variant="outline" onClick={() => sendRequest(person.email, false)} className="cancel_req hover:text-foreground hover:bg-transparent w-full"><IoMdClose /> &nbsp; Cancel Request</Button>
+                        :
+                        <>
+                          <Button onClick={() => sendRequest(person.email, true)} className="add_friend_button">
+                            <HiUserAdd /> Add Friend
+                          </Button>
+                          <Button variant="outline" className=" remove_card order ml-auto" onClick={() => removePerson(person.email)}>Remove</Button>
+                        </>
+                      }
+                    </div>
+                  ) : (
+                    <div className="mt-4">
+                      {
+                        (person.sent_friend_request) ?
+                          <Button variant="outline" onClick={() => sendRequest(person.email, false)} className="cancel_req flex gap-3 hover:text-foreground hover:bg-transparent w-full rounded-none">
+                            <IoMdClose /> Cancel Request
+                          </Button>
+                          :
+                          <Button className="add_friend_button flex gap-3 w-full border border-primary text-primary rounded-none" variant="outline" onClick={() => sendRequest(person.email, true)}>
+                            <HiUserAdd /> Add Friend
+                          </Button>
+                      }
+                    </div>
+                  )}
                 </div>
-                {withRemoveButton ? (
-                  <div className="mt-4 flex justify-between flex-1 items-end justify-between *:text-sm">
-                    {(person.sent_friend_request) ?
-                      <Button variant="outline" data-email={person.email} /*onClick={() => sendRequest(person.email, false)}*/ className="cancel_req hover:text-foreground hover:bg-transparent w-full"><IoMdClose /> &nbsp; Cancel Request</Button>
-                      :
-                      <>
-                        <Button data-email={person.email}  /*onClick={() => sendRequest(person.email, true)}*/ className="add_friend_button">
-                          <HiUserAdd /> Add Friend
-                        </Button>
-                        <Button variant="outline" data-email={person.email} className=" remove_card order ml-auto" /*onClick={() => removePerson(person.email)}*/>Remove</Button>
-                      </>
-                    }
-                  </div>
-                ) : (
-                  <div className="mt-4">
-                    {(person.sent_friend_request) ?
-                      <Button variant="outline" data-email={person.email}  /*onClick={() => sendRequest(person.email, false)}*/ className="cancel_req flex gap-3 hover:text-foreground hover:bg-transparent w-full rounded-none">
-                        <IoMdClose /> Cancel Request
-                      </Button>
-                      :
-                      <Button className="add_friend_button flex gap-3 w-full border border-primary text-primary rounded-none" variant="outline" data-email={person.email}  /*onClick={() => sendRequest(person.email, true)}*/>
-                        <HiUserAdd /> Add Friend
-                      </Button>
-                    }
-                  </div>
-                )}
-              </div>
-            </li>
-          )
-        })}
+              </motion.li>
+            )
+          })}
+        </AnimatePresence>
       </ul>
       <div className="text-center py-3 px-4 ">
         <span className="inline-flex gap-2 items-center active:bg-slate-200 p-2 mx-auto dormant-btn">See All <FaChevronRight /></span>
